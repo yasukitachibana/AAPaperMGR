969027d1a3d4ba5638065bcecb73286d8ecdaa29
Yasuki Tachibana <yasukitachibana@Yasuki-MacBook-Pro.local>
2020-08-22 11:39:39 +0900

    slurm

--- Scripts/generate_qsub_command.py
+++ Scripts/generate_qsub_command.py
@@ -2,18 +2,22 @@ import os
 
 def GenerateQsubCommand(job_name,job_command):
 
+    head = 'qsub --time=72:00:00 -p mreqp --mem=16G'
+    
+
     #head = 'qsub -V -q eamxq -l mem=16gb -l cpu_type=Intel  '
     #head = 'qsub -V -q mwsuq -l mem=32gb -l cpu_type=Intel  '
     #head = 'qsub -V -q mwsuq -l mem=16gb -l cpu_type=Intel  '
     #head = 'qsub -V -q eamxq -l mem=32gb -l cpu_type=Intel  '
     #head = 'qsub -V -q eamxq -l mem=24gb -l cpu_type=Intel  '
     #head = 'qsub -V -q wsuq -l mem=24gb -l ncpus=4 -l cpu_type=Intel  '
-    head = 'qsub -V -q wsuq -l mem=16gb -l cpu_type=Intel'
+    #head = 'qsub -V -q wsuq -l mem=16gb -l cpu_type=Intel'
     #head = 'qsub -V -q wsuq -l mem=32gb -l cpu_type=Intel'
     #head = 'qsub -V -q eamxq -l mem=32gb -l ncpus=8 -l cpu_type=Intel  '
     #head = 'qsub -V -q mwsuq -l mem=24gb -l ncpus=4 -l cpu_type=Intel  '
     #head = 'qsub -V -q eamxq -l mem=24gb -l ncpus=4 -l cpu_type=Intel  '
     out = "/dev/null"
     error =  "/dev/null"
-    log = '-N {} -o {} -e {}  -- '.format(job_name,out,error)
+    #log = '-N {} -o {} -e {}  -- '.format(job_name,out,error)
+    log = '--job-name={} -o {} -e {} '.format(job_name,out,error)
     return head+' '+log+' '+job_command
